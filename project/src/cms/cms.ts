46513ac48cbf2e961eeb7ed331e3ef76fcb010a5
tomassolanoprieto <162082823+tomassolanoprieto@users.noreply.github.com>
2024-12-09 22:54:04 +0100

    Delete project directory

--- project/src/cms/cms.ts
+++
@@ -1,9 +0,0 @@
-import CMS from 'decap-cms-app';
-import { es } from 'decap-cms-locales';
-import { config } from './config';
-
-// Register the Spanish locale
-CMS.registerLocale('es', es);
-
-// Initialize the CMS with the configuration
-CMS.init({ config });
